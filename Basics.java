d599cd6091737413d290d0757c53b53fef27cc7e
ChakaliDeepika <raptadeepika462@gmail.com>
2024-09-06 19:35:28 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,40 @@
+public class Basics{
+         public static void main(String[] args){
+                 int a=5;
+                 double b=20.5;
+                 
+                 System.out.println("main method started");
+                 System.out.println(a);
+                 System.out.println(a+b+" "+"Addition of two numbers");//arthmatic opertions
+                 System.out.println(a*b+" "+"Muliplication of two numbers");
+                 System.out.println(a++ +" "+"post-operation of a");//unary operators
+                 System.out.println(++a +" "+"pre-operation of a");
+                 System.out.println(a-- +" "+"post-decrement operation of a" );
+                 System.out.println(--a+" "+"post-increment operation of a" );
+                 System.out.println(b);
+                 for(int i=0;i<a;i++) {
+                 System.out.println(a);
+                   i++;
+                 if(a<=b || b>=a) {
+                 System.out.println("print relational operation of two numbers");
+               
+                 }
+                 
+                }
+                 
+        
+            
+             }  
+        }
+        
+        
+        
+         
+         
+
+
+          System.out.println("main method ended");
+         }
+       }    
+    
+
